0a1735db341128658ee03724435268ebea513c02
mark <mark.choi@mark-macbook.local>
2023-05-07 02:07:42 +0900

    test

--- dags/test.py
+++ dags/test.py
@@ -1,69 +1,67 @@
-from datetime import timedelta
-
-# DAG 인스턴스화에 사용하는 라이브러리
+# airflow DAG 모듈 import
 from airflow import DAG
-from kubernetes import client, config
-import ssl
-# Operator
-from airflow.operators.bash_operator import BashOperator
-from airflow.utils.dates import days_ago
+# 날짜와 시간 출력하기 위한 import
+from datetime import datetime, timedelta
 
-config = client.Configuration()
-config.api_key['authorization'] = open('/var/run/secrets/kubernetes.io/serviceaccount/token').read()
-config.api_key_prefix['authorization'] = 'Bearer'
-config.host = 'https://kubernetes.default'
-config.ssl_ca_cert = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
-config.verify_ssl=True
+# airflow 실행을 위한 PythonOperator import
+from airflow.operators.python_operator import PythonOperator
 
+# dict pass to airflow objects containing meta datas
 default_args = {
+    # owner name of the DAG
     'owner': 'airflow',
+    # whether to rely on previous task status
     'depends_on_past': False,
-    'start_date': days_ago(2),
-    # 'email': ['airflow@example.com'],
-    # 'email_on_failure': False,
-    # 'email_on_retry': False,
+    # start date of task instance
+    'start_date': datetime(2021, 3, 31),
+    # email address to get notifications
+    'email': ['abc@abc.com'],
+    # retry the task once, if it fails
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'dag': dag,
-    # 'sla': timedelta(hours=2),
-    # 'execution_timeout': timedelta(seconds=300),
-    # 'on_failure_callback': some_function,
-    # 'on_success_callback': some_other_function,
-    # 'on_retry_callback': another_function,
-    # 'sla_miss_callback': yet_another_function,
-    # 'trigger_rule': 'all_success'
+    # after waiting for 10 min
+    'retry_delay': timedelta(minutes=10),
 }
 
-
-def k8s_test(
-
-# api_client는 "2. 연결 정보 설정하기" 항목을 참고한다
-
-# 첫 번째 argument에 당신이 사용하는 namespace를 입력한다
-
-print("Listing pods with their IPs:")
-
-for i in client.CoreV1Api(client.ApiClient(config)).list_namespaced_pod("co", watch=False).items:
-    print(f"{i.status.pod_ip}\t{i.metadata.name}")
-)
-
-
-# DAG 인스턴스화
+# configure schedule & set DAG settings
 dag = DAG(
-    'k8stest',
+    dag_id='my_dag',
+    # prior tasks not executed
+    catchup = False,
     default_args=default_args,
-    description='echo "k8s test"',
-    schedule_interval=timedelta(days=1),
-
+    # how log DagRun should be up before timing out(failing)
+    # dagrun_timeout = timedelta(minutes=30),
+    # continue to run DAG once per hour
+    schedule_interval = timedelta(hours=1),
+    # schedule_interval = '*/1 * * * *', => 'M H D/M M D/W'
+    # '@daily' = '0 0 * * *'
 )
 
-t1 = BashOperator(
-    task_id='echo_k8s',
-    bash_command=k8s_test,
-    dag=dag,
+def print_time():
+    now = datetime.now()
+
+    print('=' * 20)
+    print('현재 시간은 {}입니다.'.format(now))
+    print('=' * 20)
+
+def print_day():
+    days = ['월', '화', '수', '목', '금', '토', '일']
+    days_idx = datetime.now().date().weekday()
+
+    print('=' * 20)
+    print('오늘은 {}요일 입니다.'.format(days[days_idx]))
+    print('=' * 20)
+
+
+t1 = PythonOperator(
+    task_id = 'print_time',
+    python_callable = print_time,
+    dag = dag,
 )
+
+t2 = PythonOperator(
+    task_id = 'print_day',
+    python_callable = print_day,
+    dag = dag,
+)
+
+t1.set_downstream(t2) # or t1 >> t2
